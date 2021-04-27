755327898498a4f11178346d988633dce4cfb701
Hina Firdaus <hinafirdaus95@gmail.com>
2021-04-27 16:48:02 +0530

    Add files via upload

---
+++ t4.py
@@ -0,0 +1,16 @@
+#Progra for concentric square
+from turtle import *
+
+for i in range(4):
+    forward(200)
+    left(90)
+
+penup()
+forward(30)
+left(90)
+forward(30)
+pendown()
+
+for i in range(4):
+    forward(140)
+    right(90)
